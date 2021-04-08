116107578b2d0aa612c16bbe3b7d6106503f4502
Fahad Alrasheed <fahad.alrasheed@gmail.com>
2021-04-08 15:30:13 +0300

    fetch api

--- src/Detailspage.js
+++ src/Detailspage.js
@@ -1,39 +1,43 @@
 import React from 'react';
-import {useState, useEffect} from 'react';
-import {useParams} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-function Details () {
-  const [data, setItems] = useState([]);
-    useEffect(()=> {
-    fetchDetails();
-  },);
+function Details() {
+	const [data, setItems] = useState([]);
+	useEffect(() => {
+		fetchDetails();
+	});
 
-  const {id} = useParams ();
-//let {id} = useParams ();
+	const { id } = useParams();
+	//let {id} = useParams ();
 
-  const api_url = `https://jsonplaceholder.typicode.com/users/${id}`
+	const api_url = `https://jsonplaceholder.typicode.com/users/${id}`;
 
-  const fetchDetails = async () => {
-    await fetch(api_url)
-    .then(response => response.json())
-    .then(retrievedDetails => setItems(retrievedDetails));
+	// const fetchDetails = async () => {
+	// 	await fetch(api_url)
+	// 		.then((response) => response.json())
+	// 		.then((retrievedDetails) => setItems(retrievedDetails));
+	// };
+	// console.log(data);
+
+	const fetchDetails = async function () {
+		await fetch(api_url)
+			.then((response) => response.json())
+			.then((data) => setItems(data));
+	};
+
+	const companyName = data && data.company ? data.company.name : null;
+	return (
+		<div className='detail-list'>
+			<p>-name: {`${data.name}`}</p>
+			<p>-username: {`${data.username}`}</p>
+			<p>-email: {`${data.email}`}</p>
+			<p>-phone: {`${data.phone}`}</p>
+			<p>-company: {`${companyName}`}</p>
+			<p>-website: {`${data.website}`}</p>
+			<p>-address: {`${data.address}`}</p>
+		</div>
+	);
 }
-console.log(data);
 
-  return (
-    <div className="detail-list">
-      {data.map((user) => (
-        <>
-        <p>-name{user.name}</p>
-        <p>-username{user.username}</p>
-        <p>-email{user.email}</p>
-        <p>-phone{user.phone}</p>
-        <p>-company{user.company}</p>
-        <p>-website{user.website}</p>
-        <p>-address{user.address}</p>
-        </>
-      ))}
-    </div>
-   )
-}
 export default Details;
--- src/components/card-list/card-component.js
+++ src/components/card-list/card-component.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './card-component-styles.css';
 
 export const Render = (props) => (
-  <div className = "card-list-container">
-    <ul className="ul">
-            <li className="avatar">{props.user.name[0]}</li>
-            <li className="name" >{props.user.name}</li>
-            <li className="username">@{props.user.username}</li>
-            <li className="website">http://{props.user.website}</li>
-          </ul>
-         <button className="button" key={props.user.id}>
-           <Link to={`/${props.user.id}`}>MORE DETAILS</Link>
-         </button>
-  </div>
-)
-
-
-
+	<div className='card-list-container'>
+		<ul className='ul'>
+			<li className='avatar'>{props.user.name[0]}</li>
+			<li className='name'>{props.user.name}</li>
+			<li className='username'>@{props.user.username}</li>
+			<li className='website'>http://{props.user.website}</li>
+		</ul>
+		{/* <button className='button' key={props.user.id}>
+			<Link to={`/${props.user.id}`}>MORE DETAILS</Link>
+		</button> */}
+		<Link to={`/${props.user.id}`}>
+			<button className='button' key={props.user.id}>
+				MORE DETAILS
+			</button>
+		</Link>
+	</div>
+);
