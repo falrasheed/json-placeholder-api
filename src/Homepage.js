bfcca43fff75e416b5f99b792611f3338db1518e
Fahad Alrasheed <fahad.alrasheed@gmail.com>
2021-04-11 20:14:34 +0300

    card styling

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,28 +1,35 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import {CardList} from "./components/card-list/card-list-component";
+import { CardList } from './components/card-list/card-list-component';
 
+function Card() {
+	//stored info
+	const [info, setInfo] = useState([]);
+	useEffect(() => {
+		getInfo();
+	}, []);
+	const api_url = 'https://jsonplaceholder.typicode.com/users';
+	const getInfo = async () => {
+		await fetch(api_url)
+			.then((response) => response.json())
+			.then((retrievedInfo) => setInfo(retrievedInfo));
+	};
 
-function Card () {
-  //stored info
-  const [info, setInfo] = useState([]);
-  useEffect(()=> {
-    getInfo();
-  }, []);
-  const api_url = 'https://jsonplaceholder.typicode.com/users'
-  const getInfo = async () => {
-    await fetch(api_url)
-    .then(response => response.json())
-    .then(retrievedInfo => setInfo(retrievedInfo));
-  }
-
-
-  return (
-    <div>
-      <CardList info = {info}>
-      </CardList>
-    </div>
-  );
+	return (
+		<div
+			style={{
+				backgroundImage:
+					'url(' +
+					'https://res.cloudinary.com/drrqe1a7m/image/upload/v1618156760/Integrify/jason-leung-Xaanw0s0pMk-unsplash_retery.jpg' +
+					')',
+				backgroundPosition: 'center',
+				backgroundSize: 'cover',
+				backgroundRepeat: 'no-repeat',
+			}}
+		>
+			<CardList info={info}></CardList>
+		</div>
+	);
 }
 
 export default Card;
